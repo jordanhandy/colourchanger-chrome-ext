8de7d51ff07894c55517a29881f7bf15a3e399df
Jordan Handy <handy.jordan@gmail.com>
2020-10-04 12:18:31 -0400

    change jscolor version because of compatibility errors

--- content.js
+++ content.js
@@ -10,6 +10,6 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
     if(request.todo == "changeColor")
     {
         var addColor = "#" + request.clickedColor;
-        $('.api').css('color',addColor);
+        $('.intro').css('color',addColor);
     }
 });
--- popup.js
+++ popup.js
@@ -12,8 +12,8 @@ $(function(){
         colorValue = $(this).val();
     });
     $('#btnChange').click(function(){
-        chrome.tabs.query({active:true,currentWindow:true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id,{todo: "changeColor", clickedColor: colorValue})
+        chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {todo: "changeColor", clickedColor: colorValue})
         });
     });
 });
