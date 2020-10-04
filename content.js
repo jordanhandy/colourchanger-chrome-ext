e2e99a14afd70ab0d34aac1582da7e57dd2254eb
Jordan Handy <handy.jordan@gmail.com>
2020-10-04 11:51:31 -0400

    comment on file functions

--- content.js
+++ content.js
@@ -1,6 +1,11 @@
 // send a message with the key of "todo" to show
 // the extension as being active
 chrome.runtime.sendMessage({todo: "showPageAction"});
+
+// listen for a message from the Popup.js
+// if the request comes in to "ChangeColor"
+// append a # sign to the color, so that it is valid hex
+// change the "API" class of the webpage to the newly selected color
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
     if(request.todo == "changeColor")
     {
--- popup.js
+++ popup.js
@@ -1,3 +1,11 @@
+
+// Assign the chosen color to the colorValue variable,
+// whether it is manually typed, clicked, or pasted in
+
+// when the button is selected, change the value of the items
+// in the currently active window, and send the "changeColor"
+// message to the content script in "content.js"
+
 $(function(){
     var colorValue = $('#fontColor').val();
     $('#fontColor').on("change paste keyup", function(){
