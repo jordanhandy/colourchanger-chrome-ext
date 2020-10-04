5a157e9ac1cf95ebb6cc73dd376747d9666010c0
Jordan Handy <handy.jordan@gmail.com>
2020-10-04 11:48:41 -0400

    disable custom CSS used for testing

--- content.js
+++ content.js
@@ -1,3 +1,10 @@
 // send a message with the key of "todo" to show
 // the extension as being active
 chrome.runtime.sendMessage({todo: "showPageAction"});
+chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+    if(request.todo == "changeColor")
+    {
+        var addColor = "#" + request.clickedColor;
+        $('.api').css('color',addColor);
+    }
+});
